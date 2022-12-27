a4a7af679e96718f7a99f0c758b0bcf245572620
Viacheslav Saloid <viacheslavsaloid.work@gmail.com>
2022-12-27 17:54:28 +0200

    feat: dialog

--- codegen.ts
+++ codegen.ts
@@ -1,7 +1,7 @@
 import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
-	schema: "http://192.168.68.100:3000/graphql",
+	schema: "http://192.168.68.108:3000/graphql",
 	documents: "./src/**/*.ts",
 	generates: {
 		"./graphql/generated.ts": {
--- src/environments/environment.dev.example.ts
+++ src/environments/environment.dev.example.ts
@@ -1,7 +1,7 @@
 export const environment = {
-	baseUrl: "http://192.168.68.100:4200",
-	apiUrl: "http://192.168.68.100:3000/api",
-	graphqlUrl: "http://192.168.68.100:3000/graphql",
+	baseUrl: "http://192.168.68.108:4200",
+	apiUrl: "http://192.168.68.108:3000/api",
+	graphqlUrl: "http://192.168.68.108:3000/graphql",
 	assetsUrl: "https://assets.resty.od.ua/dev/assets",
 	production: true,
 	cryptoSecret: "resty"
