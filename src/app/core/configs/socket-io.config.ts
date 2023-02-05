8983781e0ffc8644e8a52b90ba6b245d43826267
Lesha <lesha@MacBook-Pro-Lesha.local>
2023-02-05 14:10:54 +0200

    fix

--- src/app/core/configs/socket-io.config.ts
+++ src/app/core/configs/socket-io.config.ts
@@ -1,11 +1,8 @@
 import { environment } from "@env/environment";
 import type { SocketIoConfig } from "ngx-socket-io";
 
-const { hostname, port } = new URL(environment.wsUrl);
-const url = `${hostname}:${port}`;
-
 export const SOCKET_IO_CONFIG: SocketIoConfig = {
-	url,
+	url: environment.wsUrl,
 	options: {
 		autoConnect: true
 	}
