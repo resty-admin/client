48d19aef8809b4c0e0b2c58a6c5d1297b81705d6
Viacheslav Saloid <viacheslavsaloid.work@gmail.com>
2023-02-12 21:17:03 +0200

    fix: confirm products

--- src/app/core/pages/client/pages/confirm-products/resolvers/confirm-products-page/confirm-products-page.resolver.ts
+++ src/app/core/pages/client/pages/confirm-products/resolvers/confirm-products-page/confirm-products-page.resolver.ts
@@ -1,19 +1,33 @@
 import { Injectable } from "@angular/core";
 import type { Resolve } from "@angular/router";
-import type { ActivatedRouteSnapshot } from "@angular/router";
-import { PLACE_ID } from "@shared/constants";
+import { OrdersService } from "@features/orders";
+import { switchMap } from "rxjs";
 
 import { ConfirmProductsPageGQL } from "../../graphql";
 
 @Injectable({ providedIn: "root" })
 export class ConfirmProductsPageResolver implements Resolve<unknown> {
-	constructor(private readonly _confirmProductsPageGQL: ConfirmProductsPageGQL) {}
+	constructor(
+		private readonly _confirmProductsPageGQL: ConfirmProductsPageGQL,
+		private readonly _ordersService: OrdersService
+	) {}
 
-	resolve(activatedRouteSnapshot: ActivatedRouteSnapshot) {
-		const placeId = activatedRouteSnapshot.paramMap.get(PLACE_ID.slice(1)) || "";
-
-		return this._confirmProductsPageGQL.fetch({
-			filtersArgs: [{ key: "category.place.id", operator: "=", value: placeId }]
-		});
+	resolve() {
+		this._ordersService.productsToOrders$.pipe(
+			switchMap((productsToOrders) =>
+				this._confirmProductsPageGQL.fetch({
+					filtersArgs: [
+						{
+							key: "id",
+							operator: "=[]",
+							value: (productsToOrders || [])
+								.reduce((pre, productToOrder) => `${pre}${productToOrder.productId}.`, "")
+								.slice(0, -1)
+						}
+					],
+					take: productsToOrders.length
+				})
+			)
+		);
 	}
 }
