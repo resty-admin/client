1a63147e2d36a1c5967d8f865e78316542a6c739
Viacheslav Saloid <viacheslavsaloid.work@gmail.com>
2023-02-28 00:06:01 +0200

    fix: client

--- src/app/core/pages/client/pages/active-order/resolvers/active-order-page/active-order-page.resolver.ts
+++ src/app/core/pages/client/pages/active-order/resolvers/active-order-page/active-order-page.resolver.ts
@@ -1,13 +1,18 @@
 import { Injectable } from "@angular/core";
 import type { Resolve } from "@angular/router";
 import type { ActivatedRouteSnapshot } from "@angular/router";
-import { ORDER_ID } from "@shared/constants";
+import { CLIENT_ROUTES, ORDER_ID } from "@shared/constants";
+import { RouterService } from "@shared/modules/router";
+import { catchError, of } from "rxjs";
 
 import { ActiveOrderPageGQL } from "../../graphql";
 
 @Injectable({ providedIn: "root" })
 export class ActiveOrderPageResolver implements Resolve<unknown> {
-	constructor(private readonly _activeOrderPageGQL: ActiveOrderPageGQL) {}
+	constructor(
+		private readonly _activeOrderPageGQL: ActiveOrderPageGQL,
+		private readonly _routerService: RouterService
+	) {}
 
 	resolve(activatedRouteSnapshot: ActivatedRouteSnapshot) {
 		const orderId = activatedRouteSnapshot.paramMap.get(ORDER_ID.slice(1));
@@ -16,6 +21,11 @@ export class ActiveOrderPageResolver implements Resolve<unknown> {
 			return;
 		}
 
-		return this._activeOrderPageGQL.fetch({ orderId }, { fetchPolicy: "network-only" });
+		return this._activeOrderPageGQL.fetch({ orderId }, { fetchPolicy: "network-only" }).pipe(
+			catchError(async () => {
+				await this._routerService.navigateByUrl(CLIENT_ROUTES.ACTIVE_ORDERS.absolutePath);
+				return of(null);
+			})
+		);
 	}
 }
