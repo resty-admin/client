f622348dc8536e96e043c43998576bb16baf89be
Viacheslav Saloid <viacheslavsaloid.work@gmail.com>
2023-03-06 01:17:40 +0200

    fix: logs

--- src/app/core/layout/core.component.ts
+++ src/app/core/layout/core.component.ts
@@ -90,6 +90,8 @@ export class CoreComponent implements OnInit {
 	}
 
 	ngOnInit() {
+		console.log("Works");
+
 		this._router.events.subscribe((event) => {
 			if (event instanceof NavigationStart) {
 				window.strum("routeChange", event.url);
